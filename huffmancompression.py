b3bc3dd7ae3e8b4a71c0002bf3797daef9ef90ee
Chris Tao <taochris05@gmail.com>
2018-06-05 01:15:02 -0400

    general fixes

--- huffmancompression.py
+++ huffmancompression.py
@@ -19,30 +19,32 @@ class BinaryTree:
         self.listlength=len(self.sortedList)
         self.freqList=freqlist
         self.codeDict={}
+        self.nodeList=[]
     def createTree(self):
-        while len(self.sortedList)!=1:
-            if(len(self.sortedList)==self.listlength):
-                character=self.sortedList.pop(0)
-                freq=self.freqList[character]
-                node1=Node(freq,character)
-                character=self.sortedList.pop(0)
-                freq=self.freqList[character]
-                node2=Node(freq,character)
+        for char in self.sortedList:
+            self.nodeList.append(Node(self.freqList[char],char))
+        while(True):
+            for char in self.nodeList:
+                print(char.char)
+            node1=self.nodeList.pop(len(self.nodeList)-1)
+            node2=self.nodeList.pop(len(self.nodeList)-1)
+            newNode=Node(node1.frequency+node2.frequency,None)
+            if(node1.frequency>=node2.frequency):
+                newNode.left=node1
+                newNode.right=node2
             else:
-                node1=self.sortedList.pop(0)
-                character=self.sortedList.pop(0)
-                freq=self.freqList[character]
-                node2=Node(freq,character)
-            rootNode=Node(node1.frequency+node2.frequency,None)
-            if(node1.frequency>node2.frequency):
-                rootNode.left=node1
-                rootNode.right=node2
-            else:
-                rootNode.left=node2
-                rootNode.right=node1
-            self.sortedList=[rootNode]+self.sortedList
-        root=self.sortedList.pop(0)
-        return root
+                newNode.left=node2
+                newNode.right=node1
+            self.nodeList.append(newNode)
+            self.nodeList.sort(key=lambda node:node.frequency)
+            if(len(self.nodeList)==1):
+                break
+        return self.nodeList.pop(0)
+    
+    
+
+
+    
     def printTree(self,node):
         if(node.char!=None):
             print( node.char, end=" ")
@@ -53,11 +55,10 @@ class BinaryTree:
         if (rNode is None):
             return
         if (rNode.char is not None):
-            print( "%s %s" % (rNode.char, encoded))
+            print('The character is {} and the encoded is {}'.format(rNode.char, encoded))
             return
-        lencoded = encoded + "0"
-        rencoded = encoded + "1"
-        
+        lencoded = encoded + "1"
+        rencoded = encoded + "0"
         self.SearchAndCode2(rNode.left, lencoded)
         self.SearchAndCode2(rNode.right, rencoded)
 
@@ -66,12 +67,12 @@ class BinaryTree:
         if(rNode.char!=None and rNode.char==targetChar):
             self.codeDict[targetChar]=encoded
             print(encoded)
-            return 
+            return
         if(rNode.left!=None):
             self.SearchAndCode(rNode.left,targetChar,encoded+'1')
-        if(rNode.right!=None):    
+        if(rNode.right!=None):
             self.SearchAndCode(rNode.left,targetChar,encoded+'0')
-        
+
 
 
 
@@ -93,12 +94,8 @@ aList={'a':3, 'b':12, 'c':2}
 tree=BinaryTree(aList)
 aNode=tree.createTree()
 tree.SearchAndCode2(aNode,'')
-print(aNode.frequency)
-
-for char in aList:
-    print (char)
-    encodedString=''
-    tree.SearchAndCode(aNode,char, encodedString)
-    print('--------------------------------------------------')
-
-print ("done")
+#for char in aList:
+ #   print (char)
+  #  encodedString=''
+   # tree.SearchAndCode(aNode,char, encodedString)
+    #print('--------------------------------------------------')
